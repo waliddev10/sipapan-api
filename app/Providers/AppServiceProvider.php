ac3c7d180056a5d675c68f874cf985224c6b9939
walidark <mohwalidas2@gmail.com>
2022-04-26 12:12:03 +0800

    edit database setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,20 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Schema::defaultStringLength(191);
-    }
     /**
      * Register any application services.
      *
